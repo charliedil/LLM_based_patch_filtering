d3f8966bfd75bd4d5582df9aad436820cec72262
Charlie Dil <charliedil2019@gmail.com>
2024-06-13 10:32:26 -0400

    fixed scorer and split_scorer

--- split_scorer.py
+++ split_scorer.py
@@ -1,21 +1,25 @@
 import pandas as pd
 from sklearn.metrics import classification_report
 def score():
-    df = pd.read_csv("first_half_out.csv")
+    df = pd.read_csv("first_half_code_out.csv")
     df2 = pd.read_csv("first_half.csv")
 
     pred = list(df["pred"].values)
     gt = list(df2["label"].values)
 
-    df = pd.read_csv("second_half_out.csv")
+    df = pd.read_csv("second_half_code_out.csv")
     df2 = pd.read_csv("second_half.csv")
 
     pred.extend(list(df["pred"].values))
     gt.extend(list(df2["label"].values))
     
     assert len(pred)== len(gt)
+    precision = precision_score(gt, pred)
+    recall = recall_score(gt, pred)
+    f1 = f1_score(gt, pred)
+    print(f'Precision: {precision:.2f}')
+    print(f'Recall: {recall:.2f}')
+    print(f'F1 Score: {f1:.2f}')
 
-    report = classification_report(gt, pred)
-
-    print(report)
 score()
+
